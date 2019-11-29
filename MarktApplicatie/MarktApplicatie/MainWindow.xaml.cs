844de121fdb03dac2291165821f88b62fdcc1440
Soufyan <MetaByte149@gmail.com>
2019-11-29 13:22:00 +0100

    Made a basic loop and fixed formatting

--- MarktApplicatie/MarktApplicatie/MainWindow.xaml.cs
+++ MarktApplicatie/MarktApplicatie/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MarktApplicatie
 {
@@ -20,33 +21,49 @@ namespace MarktApplicatie
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer loopTimer;
+        public const int FPS = 30;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //  DispatcherTimer setup
+            loopTimer = new DispatcherTimer();
+            loopTimer.Tick += new EventHandler(Loop);
+            loopTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000/FPS);
+            loopTimer.Start();
+
+
+        }
+
+        private void Loop(Object sender, EventArgs e)
+        {
+            btn.Content += "a";
         }
 
         
 
-        private void onClick_newComposition(object sender, MouseButtonEventArgs e) {
+        private void OnClick_newComposition(object sender, MouseButtonEventArgs e) {
             EditKraam editkraam = new EditKraam();
             editkraam.Show();
             this.Close();
 
         }
-        private void onClick_oldComposition(object sender, MouseButtonEventArgs e) {
+        private void OnClick_oldComposition(object sender, MouseButtonEventArgs e) {
             OudeComposities oudecompo = new OudeComposities();
             oudecompo.Show();
             this.Close();
         }
 
-        private void onClick_statistics(object sender, MouseButtonEventArgs e) {
+        private void OnClick_statistics(object sender, MouseButtonEventArgs e) {
             statistieken statistics = new statistieken();
             statistics.Show();
             this.Close();
 
         }
 
-        private void onClick_settings(object sender, MouseButtonEventArgs e) {
+        private void OnClick_settings(object sender, MouseButtonEventArgs e) {
 
         }
 
