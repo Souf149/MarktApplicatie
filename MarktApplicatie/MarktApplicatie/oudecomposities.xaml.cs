49bce250f67bfc66328c63f212a07b922982727a
YASIN078 <47326692+YASIN078@users.noreply.github.com>
2019-12-13 19:54:11 +0100

    Update oudecomposities.xaml.cs

--- MarktApplicatie/MarktApplicatie/oudecomposities.xaml.cs
+++ MarktApplicatie/MarktApplicatie/oudecomposities.xaml.cs
@@ -25,12 +25,7 @@ namespace MarktApplicatie
         public OudeComposities()
         {
             InitializeComponent();
-            UpdateList();
-            string[] compositions = this.FindResource(composition_names) as string[];
-
-            ICollectionView view = CollectionViewSource.GetDefaultView(compositions);
-
-            new TextSearchFilter(view, this.txtFilter);
+            
 
 
             if (Settings1.Default.Darkmode == true)
@@ -83,36 +78,7 @@ namespace MarktApplicatie
             }
         }
 
-        public class TextSearchFilter
-        {
-            public TextSearchFilter(ICollectionView filteredView, TextBox textbox)
-            {
-                string filterText = "";
-
-                filteredView.Filter = delegate (object obj)
-                {
-                    if (String.IsNullOrEmpty(filterText))
-                        return true;
-
-                    string str = obj as string;
-                    if (String.IsNullOrEmpty(str))
-                        return false;
-
-                    int index = str.IndexOf(
-                        filterText,
-                        0,
-                        StringComparison.InvariantCultureIgnoreCase);
-                    return index > -1;
-
-                };
-
-                textbox.TextChanged += delegate
-                {
-                    filterText = textbox.Text;
-                    filteredView.Refresh();
-                };
-            }
-        }
+        
 
         private void AddListViewItem(string filename) {
 
