d66048b0dbba38d2785318f3354d977ae4029fbd
YASIN078 <47326692+YASIN078@users.noreply.github.com>
2020-01-09 03:05:35 +0100

    2 more changes

--- MarktApplicatie/MarktApplicatie/oudecomposities.xaml.cs
+++ MarktApplicatie/MarktApplicatie/oudecomposities.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,7 +7,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,15 +18,11 @@ namespace MarktApplicatie
     public partial class OudeComposities : Window
     {
 
-
-        public int i = 0;
         string[] composition_names;
 
         public OudeComposities()
         {
             InitializeComponent();
-
-            
 
 
             if (Settings1.Default.Font8 == true)
@@ -59,23 +53,18 @@ namespace MarktApplicatie
 
             UpdateList();
 
-            if (composition_names.Length < 1) {
-                MessageBox.Show("Je moet eerst een compositie opslaan!");
-            }
-
-            if (i == 0)
+            if (composition_names.Length < 1)
             {
-                sorttxt.Text = "Sorteer [A-Z]";
-
+                MessageBox.Show("Je moet eerst een compositie opslaan!");
             }
         }
 
-        
-
-        private void AddListViewItem(string filename) {
+        private void AddListViewItem(string filename)
+        {
 
             // add item to the list if the filename ends with .json
-            TextBlock b = new TextBlock() {
+            TextBlock b = new TextBlock()
+            {
                 Text = filename,
                 FontSize = 24
             };
@@ -84,18 +73,17 @@ namespace MarktApplicatie
 
         }
 
-
-        
         private void ListView_onclick(object sender, MouseButtonEventArgs e)
         {
-            if (listView.SelectedIndex < 0) {
+            if (listView.SelectedIndex < 0)
+            {
                 return;
             }
 
             TextBlock txt = (TextBlock)listView.SelectedItem;
             string img_filename = txt.Text + ".png";
 
-        
+
 
             BitmapImage image = new BitmapImage();
             image.BeginInit();
@@ -109,26 +97,18 @@ namespace MarktApplicatie
 
         }
 
-        private void onClick_homepage(object sender, RoutedEventArgs e) {
+        private void onClick_homepage(object sender, RoutedEventArgs e)
+        {
             MainWindow home = new MainWindow();
             home.Show();
             Close();
         }
 
-        private void statistic(object sender, RoutedEventArgs e)
+        public void StartSketch(object sender, RoutedEventArgs e)
         {
+
             if (listView.SelectedIndex < 0)
             {
-                MessageBox.Show("Kies eerst een compositie!", "Zie statistiek", MessageBoxButton.OK, MessageBoxImage.Information);
-                return;
-            }
-            new statistieken_yasin().Show();
-            this.Close();
-        }
-
-        public void StartSketch(object sender, RoutedEventArgs e) {
-
-            if (listView.SelectedIndex < 0) {
                 return;
             }
 
@@ -139,8 +119,10 @@ namespace MarktApplicatie
 
             EditKraam editkraam = new EditKraam();
 
-            if (result != null) {
-                foreach (PlankInfo p in result) {
+            if (result != null)
+            {
+                foreach (PlankInfo p in result)
+                {
                     editkraam.Add_plank(p.X, p.Y, p.Width, p.Height);
                 }
             }
@@ -149,19 +131,22 @@ namespace MarktApplicatie
             Close();
         }
 
-      
 
 
-        public void btnEditTitle_Click(object sender, RoutedEventArgs e) {
+
+        public void btnEditTitle_Click(object sender, RoutedEventArgs e)
+        {
 
             int selected_index = listView.SelectedIndex;
-            if (selected_index < 0) {
+            if (selected_index < 0)
+            {
                 return;
             }
 
             save_popup popup = new save_popup();
 
-            if (popup.ShowDialog() == true) {
+            if (popup.ShowDialog() == true)
+            {
                 string compositions_path = SoufTools.compositions_path;
 
                 string old_filename = composition_names[selected_index];
@@ -170,7 +155,7 @@ namespace MarktApplicatie
                 string old_path = Path.Combine(compositions_path, old_filename);
                 string new_path = Path.Combine(compositions_path, new_filename);
 
-                
+
                 File.Move(old_path + ".png", new_path + ".png");
                 File.Move(old_path + ".json", new_path + ".json");
 
@@ -178,93 +163,47 @@ namespace MarktApplicatie
             }
         }
 
-        private void UpdateList() {
+        private void UpdateList()
+        {
 
             listView.Items.Clear();
 
             string[] files = SoufTools.GetAllCompositions();
             composition_names = new string[files.Length / 2];
 
-            
+
             for (int i = 0, j = 0; i < files.Length; i++)
             {
-                if(Path.GetExtension(files[i]) == ".json")
+                if (Path.GetExtension(files[i]) == ".json")
                 {
                     composition_names[j++] = Path.GetFileNameWithoutExtension(files[i]);
                 }
             }
-            
+
             // for every file get the filename and add it to 
-            foreach (string name in composition_names) {
-                    AddListViewItem(name);
+            foreach (string name in composition_names)
+            {
+                AddListViewItem(name);
             }
 
 
         }
 
-
-        private void Sort(object sender, RoutedEventArgs e)
+        private void OnClick_Verwijder(object sender, RoutedEventArgs e)
         {
 
-            i += 1;
-            if (i == 2)
+            if (listView.SelectedIndex < 0)
             {
-                i = 0;
-                sorttxt.Text = "Sorteer [A-Z]";
-                listView.Items.SortDescriptions.Clear();
-                // sorteer z-a
-            }
-
-            if (i == 1)
-            {
-                sorttxt.Text = "Sorteer [Z-A]";
-                // sorteer a-z
-            }
-
-            
-
-
-        }
-
-        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            string inp = txtFilter.Text;
-            if (inp == "")
-            {
-                UpdateList();
-                return;
-            }
-            UpdateList();
-
-            ItemCollection old_listView_list = listView.Items;
-            List<string> new_listView_list = new List<string>();
-            foreach (TextBlock txt_block in old_listView_list)
-            {
-                string txt = txt_block.Text;
-                if (txt.ToLower().Contains(inp.ToLower()))
-                {
-                    new_listView_list.Add(txt);
-                }
-            }
-            listView.Items.Clear();
-            foreach (string txt in new_listView_list)
-            {
-                AddListViewItem(txt);
-            }
-        }
-
-
-        private void OnClick_Verwijder(object sender, RoutedEventArgs e) {
-
-            if (listView.SelectedIndex < 0) {
                 return;
             }
 
 
             confirmation_popup popup = new confirmation_popup();
 
-            if (popup.ShowDialog() == true) {
-                if (popup.Confirmation) {
+            if (popup.ShowDialog() == true)
+            {
+                if (popup.Confirmation)
+                {
                     string filename = composition_names[listView.SelectedIndex];
                     File.Delete(Path.Combine(SoufTools.compositions_path, filename + ".json"));
                     File.Delete(Path.Combine(SoufTools.compositions_path, filename + ".png"));
